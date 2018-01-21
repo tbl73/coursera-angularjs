6fe9f48d070b9569eea3ab90bcec8fad68848036
Trisha Lewis <trishablewis@gmail.com>
2018-01-21 11:04:59 -0500

    Module 1 solution

---
+++ mod-1-solution/app.js
@@ -0,0 +1,37 @@
+(function(){
+'use strict';
+
+angular.module('LunchCheckApp', [])
+.controller('LunchCheckController', LunchCheckController)
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+  $scope.items = "";
+  $scope.message = "";
+
+  $scope.checkLunch = function(){
+    var msg = getMessage($scope.items);
+    $scope.message = msg;
+  };
+
+  function getMessage(string){
+    var msg = "";
+    //get array from string then remove empty items
+    var items = string.split(',').filter(function(x){
+      return (x.trim() != "");
+    });
+    var length = items.length;
+    //set message according to array length
+    if (length == 0) {
+      msg = "Please enter data first.";
+    } else if (length >= 1 && length <= 3) {
+        msg = "Enjoy!";
+    } else {
+        msg = "Too much!"
+    };
+    return msg;
+  };
+}
+
+})();
